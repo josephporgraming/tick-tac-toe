64621a037d0ce0584b149814dd8777341e0e3a8a
josephporgraming <125146238+josephporgraming@users.noreply.github.com>
2023-02-26 13:21:36 -0500

    code

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,13 +11,30 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             string[,] board = new string[3, 3];
-
+            string input;
+            string input2;
+            bool isx;
         }
-        public int inputforbourd(string cords, string cords2)
+        public int inputforbourd(string cords)
         {
             int courds1 = Int32.Parse(cords);
+            return courds1;
+        }
+        public int inputforbourd2(string cords2)
+        {
             int courds2 = Int32.Parse(cords2);
-            return courds1 + courds2;
+            return courds2;
+        }
+        public int cheackforwin(int firstnuber, int secondnuber)
+        {
+            int[,] win = new int[firstnuber ,secondnuber];
+            
+            return ;
+        }
+        public int cheackforcolison(string[,] bourdforcheack)
+        {
+
+            return;
         }
     }
 }
